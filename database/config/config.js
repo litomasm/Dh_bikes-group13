5de30a803cfa53ba9c1713fa294cd8387a28943b
Diegos34 <diegosabater_34@hotmail.com>
2021-03-02 20:43:17 -0300

    'configurarndo BD'

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports= {
   "development": {
     "username": "root",
     "password": null,
-    "database": "dhbikesdb",
+    "database": "dhbikesv2",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "define": {
